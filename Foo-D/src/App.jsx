3a9d501cc62d9f28b85be0942ebcbb0fe7db5b83
CodeMonitor-lab <niteshcse3333@gmail.com>
2025-07-23 22:22:56 +0300

    Adding changes

--- Foo-D/src/App.jsx
+++ Foo-D/src/App.jsx
@@ -3,12 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import AppRoute from './Routes/AppRoute'
 import './App.css'
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <Analytics />
     <AppRoute/>
     </>
   )
